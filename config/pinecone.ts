5f0c1f90dc6c65a0df62bd39f63ff4b085c2bc7e
michaelwiltfong-okta <michael.wiltfong@okta.com>
2023-05-03 14:45:11 -0400

    base template prompt in md

--- config/pinecone.ts
+++ config/pinecone.ts
@@ -11,4 +11,3 @@ const PINECONE_INDEX_NAME = process.env.PINECONE_INDEX_NAME ?? '';
 const PINECONE_NAME_SPACE = 'first-test'; //namespace is optional for your vectors
 
 export { PINECONE_INDEX_NAME, PINECONE_NAME_SPACE };
-``
